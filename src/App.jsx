1ecb7a518a3695e66dfe26edb93ff9ea013f891f
Ontiveroshtml <velko.lords@gmail.com>
2025-11-21 19:31:26 -0600

    Add front-end style guide with layout, colors, and typography specifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+
+  // ⬅️ Cargar desde localStorage SIN useEffect
+  const [item, setItem] = useState(() => {
+    return JSON.parse(localStorage.getItem("tasks") || "[]");
+  });
+
+  // ⬅️ Guardar en localStorage cuando cambien las tareas
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(item));
+  }, [item]);
+
+  const getTask = (e) => {
+    setTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (task.trim() !== "") {
+      setItem([...item, task]);
+    }
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    const filterTask = item.filter((_, i) => i !== index);
+    setItem(filterTask);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>TODO APP</h1>
+
+      <input
+        type="text"
+        value={task}
+        onChange={getTask}
+        className="bg-Purple-600 text-white"
+      />
+      <button onClick={addTask}>Agregar</button>
+
+      {item.map((t, index) => (
+        <div key={index} className="flex bg-Navy-900 text-white gap-8">
+          <p>{t}</p>
+          <button className="bg-green-700" onClick={() => deleteTask(index)}>
+            Completar
+          </button>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
